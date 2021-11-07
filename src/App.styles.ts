52a3ef423027ebadb81a5b753c9b0ce8228d85a3
Armen_Baldryan <Armen_Baldryan@mmsi.com>
2021-11-07 21:21:47 +0400

    Removing unnecessary files and adding styles and material ui

---
+++ src/App.styles.ts
@@ -0,0 +1,15 @@
+import { styled } from "@mui/material/styles";
+
+export const Wrapper = styled("div")(
+	() => `
+    margin: 50px auto;
+    max-width: 1000px
+  `
+);
+
+export const Header = styled("h2")(
+	() => `
+    display: flex;
+    justify-content: center;
+  `
+);
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Wrapper, Header } from "./App.styles";
+import GlobalStyles from "@mui/material/GlobalStyles";
+import { blueGrey } from "@mui/material/colors";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Wrapper>
+			<GlobalStyles
+				styles={{
+					body: { backgroundColor: blueGrey[200] },
+				}}
+			/>
+			<Header>Exchange rates</Header>
+		</Wrapper>
+	);
 }
 
 export default App;
